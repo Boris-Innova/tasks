2809c816385be4682e6fc378f801e06f0c787ec5
BorisChernykh <b.chernykh@sape.ru>
2025-04-05 14:57:54 +0500

    main: 5_pig_latin.php

---
+++ 50_excercies_lerner_renuven/5_pig_latin.php
@@ -0,0 +1,30 @@
+<?php
+
+const EN_LETTERS = [
+    'a', 'e', 'i', 'o', 'u', 'y',
+    'A', 'E', 'I', 'O', 'U', 'Y'
+];
+
+function convert_word_to_pig_latin(string $word): string
+{
+    $word = trim($word);
+    if (empty($word)) {
+        return '';
+    }
+
+    $firstLetter = $word[0];
+    if (in_array($firstLetter, EN_LETTERS)) {
+        $wordInPigLatinFormat = $word . 'way';
+    } else {
+        $wordInPigLatinFormat = substr_replace($word, '', 0, 1) . $firstLetter . 'ay';
+    }
+
+    return $wordInPigLatinFormat;
+}
+
+
+$wordsForTest = ['fuck', 'donal', 'GAlexandr_', 'apple', 'any', 'x', 'a'];
+foreach ($wordsForTest as $wordForTest) {
+    $result = convert_word_to_pig_latin($wordForTest);
+    print_r("WORD FOR TEST: $wordForTest, RESULT: $result \n");
+}
